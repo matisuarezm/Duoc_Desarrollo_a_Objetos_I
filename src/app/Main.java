4ea1982a03278b732d073a48015651b8eb76f7ee
matisuarezm <mati.suarez@duocuc.cl>
2025-11-07 15:40:32 -0300

    Limpieza y pruebas, se deja version final

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,7 +4,6 @@ package app;
 import model.Direccion;
 import model.Empleado;
 import model.Persona;
-import model.Rut;
 import util.ValidaRutException;
 
 /**
@@ -25,56 +24,56 @@ public class Main {
     public static void main(String[] args){
 
         try {
-            //Objeto direccion y Empleado, se imprime información completa
+            //Objetos Direccion y Empleado, donde pasamos parametro direccion a empleado.
             Direccion direccion = new Direccion("Avenida Concepcion", 1287, "Depto 503", "Colina", "RM");
-            Persona empleado = new Empleado("Matias", "Suarez", "17405315-7", direccion,"+56965995289",
+            Empleado empleado = new Empleado("Matias", "Suarez", "17405315-7", direccion,"+56965995289",
                     "20-01-2020", "Encargado TI","Informatica", "mati.suarez@duocuc.cl", 5000);
             System.out.printf(empleado.toString());
 
             System.out.println("-----------------------------");
 
-            //Objeto empleado con constructor completo
+            //Objeto empleado con constructor completo, definimos todos los campos en objeto empleado
             Persona empleado2 = new Empleado("Oriana", "Muñoz", "6068555-9", "Nuñez de Balboa", 1123,"Casa 15",
-                    "Santiago", "RM", "+56960524879", "20-01-2020", "Encargado TI","Informatica", "mati.suarez@duocuc.cl", 3000);
+                    "Santiago", "RM", "+56960524879", "20-01-2020", "Encargado TI","Informatica", "orianamunoz@mail.cl", 3000);
             System.out.printf(empleado2.toString());
 
             System.out.println("-----------------------------");
 
-            //Objeto empleado
+            //Objeto empleado, direccion, teléfono y mail vacíos. Estos para probar el toString()
             Persona empleado3 = new Empleado("Victoria", "Suarez", "26680048-7", null,"", "20-01-2020", "Encargado TI",
                     "Informatica", "", 2000);
             System.out.printf(empleado3.toString());
 
             System.out.println("-----------------------------");
 
-            //Objeto empleado con campos nulos o vacios
-            Persona empleado4 = new Empleado("Matias", "Suarez", "17404347-7", null,
-                    null, "20-01-2020", "Encargado TI",
+            //Objeto empleado con campos nulos o vacíos.
+            Persona empleado4 = new Empleado("Matias", "Suarez", "17404347-7", null, null, "20-01-2020", "",
                     "Informatica", "", 1000);
             System.out.printf(empleado4.toString());
 
             System.out.println("-----------------------------");
 
-            //Objeto empleado con campos nulos o vacios y sueldo negativo (arroja excepción)
-            Persona empleado5 = new Empleado("Matias", "Suarez", "17404347-7", null,
-                    null, "20-01-2020", "Encargado TI",
-                    "Informatica", "", 1000);
-            System.out.printf(empleado4.toString());
+            //Creación de objetos con setters
+            Direccion direccion2 = new Direccion();
+            direccion2.setCalle("Avenida");
+            direccion2.setNumero(2589);
+            direccion2.setCasaDepartamento("Parcela 3");
+            direccion2.setComuna("Runge");
+            direccion2.setRegion("XIII");
 
-            System.out.println("-----------------------------");
-
-            Direccion direccion2 = new Direccion("Avenida",123,null,"runge","RM");
-            Persona persona = new Empleado();
+            Persona persona = new Empleado(); //Al instancia a Persona y no Empleado, no deja utilizar los atributos de Empleado en los setters
             persona.setNombre("Diego");
             persona.setApellido("Varela");
             persona.setRut("15682541-7");
             persona.setTelefono("+56987456213");
             persona.setDireccion(direccion2);
 
+
             System.out.println(persona);
 
             System.out.println("-----------------------------");
 
+            //Se utiliza Instancia Empleado y se agregan todos los campos.
             Empleado persona1 = new Empleado();
             persona1.setNombre("Luis");
             persona1.setApellido("Dinamarca");
@@ -88,9 +87,6 @@ public class Main {
             persona1.setSueldo(7000);
 
             System.out.println(persona1);
-
-            Rut r = new Rut("123456-8");
-
 
         } catch (ValidaRutException | IllegalArgumentException e) {
             System.out.println(e.getMessage());
